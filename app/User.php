b0c3b4cecf46c1e496aef55a29f47864d2ee2f9d
aleyn-ars <arslanaleyn@gmail.com>
2020-09-18 22:23:26 +0300

    first commit

---
+++ app/User.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    protected $fillable=[
+        'name','username','email','password'//doldurabilir alanlar null geçme anlamında
+    ];
+}
